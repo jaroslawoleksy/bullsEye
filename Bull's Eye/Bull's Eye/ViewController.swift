300db0053d658b05e4ca1bc4ff4fbe16ab4e47d8
jaroslawoleksy <jaroslaw.t.oleksy@aleabroker.pl>
2018-01-22 19:44:27 +0100

    Bull's Eye
    Ray Weinerlich app

---
+++ Bull's Eye/Bull's Eye/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  Bull's Eye
+//
+//  Created by Jaroslaw Oleksy on 28.11.2017.
+//  Copyright © 2017 codePark. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ViewController: UIViewController {
+
+  var currentValue = 0
+  var targetValue = 0
+  var score = 0
+  var round = 0
+  
+  @IBOutlet weak var slider: UISlider!
+  @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var roundLabel: UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    startNewGame()
+    
+    let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+    slider.setThumbImage(thumbImageNormal, for: .normal)
+    
+    let thumbImageHighlited = UIImage(named: "SliderThumb-Highlighted")!
+    slider.setThumbImage(thumbImageHighlited, for: .highlighted)
+    
+    let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+    
+    let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+    let trackLeftResizable = trackLeftImage.resizableImage(withCapInsets: insets)
+    slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+    
+    let trackRightImage = UIImage(named: "SliderTrackRight")!
+    let trackRightResizable = trackRightImage.resizableImage(withCapInsets: insets)
+    slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  @IBAction func hitMePressed(_ sender: Any) {
+    let difference = abs(currentValue - targetValue)
+    var points = 100 - difference
+    
+    let title: String
+    if difference == 0 {
+      title = "Perfect!"
+      points += 100
+    } else if difference < 5 {
+      title = "You almost had it!"
+      if difference == 1 {
+        points += 50
+      }
+    } else if difference < 10 {
+      title = "Pretty good!"
+    } else {
+      title = "Not even close..."
+    }
+    
+    score += points
+    
+    let message = "You scored \(points) points"
+    let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    let action = UIAlertAction(title: "OK", style: .default, handler: {action in self.startNewRound()})
+    
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+    //startNewRound()
+  }
+  
+  @IBAction func sliderMoved(_ slider: UISlider) {
+    currentValue = lroundf(slider.value)
+    print("The slider position is: \(currentValue)")
+  }
+  
+  @IBAction func startNewGame() {
+    round = 0
+    score = 0
+    startNewRound()
+    let transition = CATransition()
+    transition.type = kCATransitionFade
+    transition.duration = 1
+    transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+    view.layer.add(transition, forKey: nil)
+  }
+  
+  func startNewRound() {
+    targetValue = 1 + Int(arc4random_uniform(100))
+    currentValue = 50
+    slider.value = Float(currentValue)
+    round += 1
+    updateLabels()
+  }
+  
+  func updateLabels() {
+    targetLabel.text = String(targetValue)
+    scoreLabel.text = String(score)
+    roundLabel.text = String(round)
+  }
+
+}
+
